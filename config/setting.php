db10dbf054bf03d2a99fcc4f2e30cd9cfe7f3d86
Bill <bill@robowebtech.com>
2023-02-10 00:30:34 +0800

    fix title size

--- config/setting.php
+++ config/setting.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'post_title_size' => 100,
+    'post_title_size' => 50,
     'post_description_size' => 300,
     'post_min_element_count' => 8,
     'post_max_element_count' => 1024,
