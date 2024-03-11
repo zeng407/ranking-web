ebe6ec10355a45eb59f1d0791af92dcc12f70b82
bill <bill@robowebtech.com>
2024-03-11 20:21:18 +0800

    Add GitHub link to footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-center text-white mt-auto">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © {{now()->year}} Copyright <a class="text-white">{{config('app.url')}}</a> | Email {{config('app.contact_email')}}
+    © {{now()->year}} Copyright <a class="text-white">{{config('app.url')}}</a> | Email {{config('app.contact_email')}} | Github <a class=" text-white" href="https://github.com/zeng407/ranking-web"><i class="fab fa-github"></i></a>
   </div>
 </footer>
