d962f916ca254d6952c44401d9dc9aa10fab4667
bill <bill@robowebtech.com>
2024-03-11 14:59:29 +0800

    Update contact email in .env.example and footer.blade.php

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-center text-white mt-auto">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © {{now()->year}} Copyright <a class="text-white">{{config('app.url')}}</a> | Email 2pick.cc@gmail.com
+    © {{now()->year}} Copyright <a class="text-white">{{config('app.url')}}</a> | Email {{config('app.contact_email')}}
   </div>
 </footer>
