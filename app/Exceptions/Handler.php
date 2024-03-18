612e8a4edd486da966e25d7512be28fe7f6d5043
bill <bill@robowebtech.com>
2024-03-18 23:26:51 +0800

    Add exception handling for ThrottleRequestsException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Support\Facades\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof \Illuminate\Http\Exceptions\ThrottleRequestsException) {
+            return Response::json(['message' => __('Too Many Requests')], 429);
+        }
+
+        return parent::render($request, $exception);
     }
 }
