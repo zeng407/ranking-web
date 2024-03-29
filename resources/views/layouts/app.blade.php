2be9d2580d62aac35a197cc5667f94aaf60d1c5c
bill <bill@robowebtech.com>
2024-03-30 02:03:27 +0800

    Update asset path for JavaScript file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Scripts --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js')  }}" defer></script>
 
     {{-- Fonts --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
