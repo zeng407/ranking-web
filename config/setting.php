3fdc13f494edd07d77ecd136d89b6a059b27054e
bill <bill@robowebtech.com>
2024-04-04 11:19:55 +0800

    Update post_max_element_count to 256

--- config/setting.php
+++ config/setting.php
@@ -7,7 +7,7 @@ return [
     'post_title_size' => 50,
     'post_description_size' => 300,
     'post_min_element_count' => 8,
-    'post_max_element_count' => 128,
+    'post_max_element_count' => 256,
     'element_title_size' => 100,
     'post_max_tags' => 5,
     'tag_name_size' => 15,
