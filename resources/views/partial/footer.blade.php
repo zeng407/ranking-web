05ba2baa9ef60e33074112b0de55b419a804a151
bill <bill@robowebtech.com>
2024-04-06 22:01:42 +0800

    Add social configuration and Facebook link to footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="bg-dark text-center text-white mt-auto">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © {{now()->year}} Copyright <a class="text-white">{{config('app.url')}}</a> | Email {{config('app.contact_email')}} 
+    © {{now()->year}} Copyright <a class="text-white" href="{{config('app.url')}}">{{config('app.url')}}</a> | Email <a class="text-white" href="mailto:{{config('app.contact_email')}}">{{config('app.contact_email')}}</a>
+    @if(config('social.show_facebook'))| <a href="{{config('social.facebook_page_url')}}" target="_blank" class="text-white"><i class="fab fa-facebook-f"></i> Facebook</a> @endif
   </div>
 </footer>
