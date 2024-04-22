5716486181030ebfeff8b05d34fd076fc78d4b86
bill <bill@robowebtech.com>
2024-04-23 05:57:05 +0800

    Refactor script_load_ad.blade.php to include a retry mechanism for loading ads

--- resources/views/ads/script_load_ad.blade.php
+++ resources/views/ads/script_load_ad.blade.php
@@ -1,19 +1,22 @@
 <script>
-    let retry = 10;
-    let interval = setInterval(() => {
-        try {
-            // console.log('try to push ad, try: ' + retry);
-            retry--;
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (e) {
-            if (e.message.includes(
-                    `All 'ins' elements in the DOM with class=adsbygoogle already have ads in them`)) {
-                clearInterval(interval);
-                return;
+    function loadAd(retryCount, intervalDelay) {
+        let retry = retryCount;
+        let interval = setInterval(() => {
+            try {
+                // console.log('try to push ad, try: ' + retry);
+                retry--;
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            } catch (e) {
+                if (e.message.includes(`All 'ins' elements in the DOM with class=adsbygoogle already have ads in them`)) {
+                    clearInterval(interval);
+                    return;
+                }
             }
-        }
-        if (retry <= 0) {
-            clearInterval(interval);
-        }
-    }, 500);
+            if (retry <= 0) {
+                clearInterval(interval);
+            }
+        }, intervalDelay);
+    }
+
+    loadAd(10, 500);
 </script>
