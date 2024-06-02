f3cda3f327c9ef03ced6d25182917fcf75a44ca5
bill <bill@robowebtech.com>
2024-06-02 17:38:39 +0800

    feat: Update home container height and remove redundant code

--- resources/views/game/partial/global-element-container.blade.php
+++ resources/views/game/partial/global-element-container.blade.php
@@ -1,9 +1,5 @@
-@php
-if(!isset($height)) $height = 270
-@endphp
-
 @if ($rank->element->type === 'video' && $rank->element->video_source === 'youtube')
-    <youtube-player width="100%" height="{{$height}}" ref-id="{{ $rank->element->id }}"
+    <youtube-player width="100%" ref-id="{{ $rank->element->id }}"
         video-id="{{ $rank->element->video_id }}" :controls="true" :autoplay="false" :rel="0"
         origin="{{ request()->getSchemeAndHttpHost() }}">
     </youtube-player>
@@ -12,25 +8,24 @@ if(!isset($height)) $height = 270
 @elseif($rank->element->type === 'video' && $rank->element->video_source === 'twitch_video')
     <iframe
         src="https://player.twitch.tv/?video={{ $rank->element->video_id }}&parent={{ request()->getHost() }}&autoplay=false"
-        height="{{$height}}" width="100%" allowfullscreen>
+        width="100%" allowfullscreen>
     </iframe>
 @elseif($rank->element->type === 'video' && $rank->element->video_source === 'twitch_clip')
     <iframe
         src="https://clips.twitch.tv/embed?clip={{ $rank->element->video_id }}&parent={{ request()->getHost() }}&autoplay=false"
-        height="{{$height}}" width="100%" allowfullscreen>
+        width="100%" allowfullscreen>
     </iframe>
 @elseif ($rank->element->type === 'video' && $rank->element->video_source === 'bilibili_video')
-    <bilibili-video width="100%" height="{{$height}}"
+    <bilibili-video width="100%"
         :element="{{ json_encode(['id' => $rank->element->id, 'video_id' => $rank->element->video_id, 'thumb_url' => $rank->element->thumb_url]) }}"
-        :autoplay="false" :muted="false" height="{{$height}}" :preview="true">
+        :autoplay="false" :muted="false" :preview="true">
     </bilibili-video>
 @elseif($rank->element->type === 'video')
-    <video width="100%" height="{{$height}}" loop controls playsinline src="{{ $rank->element->source_url }}" poster="{{ $rank->element->thumb_url }}"></video>
+    <video width="100%" loop controls playsinline src="{{ $rank->element->source_url }}" poster="{{ $rank->element->thumb_url }}"></video>
 @elseif($rank->element->type === 'image')
     <viewer :options="viewerOptions">
-        <img class="w-auto mw-100 cursor-pointer"
+        <img class="mw-100 cursor-pointer"
           src="{{ $rank->element->getScaledThumbUrl()}}"
-          height="{{$height}}"
           alt="{{ $rank->element->title }}">
     </viewer>
 @endif
--- resources/views/game/partial/my-champion-container.blade.php
+++ resources/views/game/partial/my-champion-container.blade.php
@@ -1,5 +1,5 @@
 @if ($gameResult->winner->type === 'video' && $gameResult->winner->video_source === 'youtube')
-    <youtube-player width="100%" height="270" ref-id="{{ $gameResult->winner->id }}"
+    <youtube-player width="100%" ref-id="{{ $gameResult->winner->id }}"
         video-id="{{ $gameResult->winner->video_id }}" :controls="true" :autoplay="false"
         :rel="0" origin="{{ request()->getSchemeAndHttpHost() }}">
     </youtube-player>
@@ -7,24 +7,24 @@
     {!! inject_youtube_embed($gameResult->winner->source_url, ['autoplay' => false]) !!}
 @elseif($gameResult->winner->type === 'video' && $gameResult->winner->video_source === 'twitch_video')
     <iframe src="https://player.twitch.tv/?video={{ $gameResult->winner->video_id }}&parent={{ request()->getHost() }}"
-        height="270" width="100%" allowfullscreen>
+        width="100%" allowfullscreen>
     </iframe>
 @elseif($gameResult->winner->type === 'video' && $gameResult->winner->video_source === 'twitch_clip')
     <iframe
         src="https://clips.twitch.tv/embed?clip={{ $gameResult->winner->video_id }}&parent={{ request()->getHost() }}&autoplay=false"
-        height="270" width="100%" allowfullscreen>
+        width="100%" allowfullscreen>
     </iframe>
 @elseif($gameResult->winner->type === 'video' && $gameResult->winner->video_source === 'bilibili_video')
-    <bilibili-video width="100%" height="270"
+    <bilibili-video width="100%"
         :element="{{ json_encode(['id' => $gameResult->winner->id, 'video_id' => $gameResult->winner->video_id, 'thumb_url' => $gameResult->winner->thumb_url]) }}"
-        :autoplay="false" :muted="false" height="270" :preview="true">
+        :autoplay="false" :muted="false"  :preview="true">
     </bilibili-video>
 @elseif($gameResult->winner->type === 'video')
-    <video width="100%" height="270" loop controls playsinline src="{{ $gameResult->winner->source_url }}" poster="{{ $gameResult->winner->thumb_url }}"></video>
+    <video width="100%"  loop controls playsinline src="{{ $gameResult->winner->source_url }}" poster="{{ $gameResult->winner->thumb_url }}"></video>
 @elseif($gameResult->winner->type === 'image')
     <viewer :options="viewerOptions">
         <img class="w-auto mw-100 cursor-pointer my-champion-element"
-          src="{{ $gameResult->winner->getScaledThumbUrl() }}"
-            alt="{{ $gameResult->winner->title }}">
+          src="{{ $gameResult->winner->lowthumb_url ?: $gameResult->winner->thumb_url }}"
+          alt="{{ $gameResult->winner->title }}">
     </viewer>
 @endif
