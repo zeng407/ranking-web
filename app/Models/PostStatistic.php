ee86741eec4e42583453dc696501fbeff418b6d7
bill <bill@robowebtech.com>
2024-06-05 11:36:40 +0800

    feat: create post_statistics table

---
+++ app/Models/PostStatistic.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostStatistic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'start_date',
+        'play_count'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
