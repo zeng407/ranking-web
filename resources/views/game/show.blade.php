a47dfee376b707ec1dae9ac047030c37405025f1
bill <bill@robowebtech.com>
2024-06-29 19:11:13 +0800

    feat: Update vote count display in game show page
    Refactor the code in the game show page to update the display of vote counts using the `<I-Count-Up>` component. This improves the user experience by animating the vote count changes.

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -186,7 +186,8 @@
                         @click="leftWin">
                         <i class="fa-solid fa-2x fa-thumbs-up"></i>
                         <h3 class="d-inline-block m-0" v-if="showGameRoomVotes">
-                          @{{ leftVotes }}&nbsp;(@{{ leftVotesPercentage}}%)
+                          <I-Count-Up :end-val="leftVotes"></I-Count-Up>
+                          (<I-Count-Up :end-val="leftVotesPercentage"></I-Count-Up>%)
                         </h3>
                       </button>
                       <div class="row" v-if="isYoutubeSource(le) || isVideoUrlSource(le)">
@@ -202,7 +203,8 @@
                           <button class="btn btn-primary btn-lg btn-block d-block d-sm-none" :disabled="isVoting"
                             @click="leftWin"><i class="fa-solid fa-thumbs-up"></i>
                             <p class="d-inline-block m-0" v-if="showGameRoomVotes">
-                              @{{ leftVotes }}&nbsp;(@{{ leftVotesPercentage}}%)
+                              <I-Count-Up :end-val="leftVotes"></I-Count-Up>
+                              (<I-Count-Up :end-val="leftVotesPercentage"></I-Count-Up>%)
                             </p>
                           </button>
                         </div>
@@ -212,7 +214,8 @@
                         <button class="btn btn-primary btn-block btn-lg d-block d-md-none" :disabled="isVoting"
                           @click="leftWin"><i class="fa-solid fa-2x fa-thumbs-up"></i>
                           <p class="d-inline-block m-0" v-if="showGameRoomVotes">
-                            @{{ leftVotes }}&nbsp;(@{{ leftVotesPercentage}}%)
+                            <I-Count-Up :end-val="leftVotes"></I-Count-Up>
+                            (<I-Count-Up :end-val="leftVotesPercentage"></I-Count-Up>%)
                           </p>
                         </button>
                       </div>
@@ -296,7 +299,8 @@
                       <button id="right-btn" class="btn btn-danger vote-button btn-block d-none d-sm-block" :disabled="isVoting"
                         @click="rightWin"><i class="fa-solid fa-2x fa-thumbs-up"></i>
                         <h3 class="d-inline-block m-0" v-if="showGameRoomVotes">
-                          @{{ rightVotes }}&nbsp;(@{{ rightVotesPercentage }}%)
+                          <I-Count-Up :end-val="rightVotes"></I-Count-Up>
+                          (<I-Count-Up :end-val="rightVotesPercentage"></I-Count-Up>%)
                         </h3>
                       </button>
                       <div class="row" v-if="isYoutubeSource(re) || isVideoUrlSource(re)">
@@ -312,7 +316,8 @@
                           <button class="btn btn-danger btn-lg btn-block d-block d-sm-none" :disabled="isVoting"
                             @click="rightWin"><i class="fa-solid fa-thumbs-up"></i>
                             <p class="d-inline-block m-0" v-if="showGameRoomVotes">
-                              @{{ rightVotes }}&nbsp;(@{{ rightVotesPercentage }}%)
+                              <I-Count-Up :end-val="rightVotes"></I-Count-Up>
+                              (<I-Count-Up :end-val="rightVotesPercentage"></I-Count-Up>%)
                             </p>
                           </button>
                         </div>
@@ -321,7 +326,8 @@
                         <button class="btn btn-danger btn-lg btn-block d-block d-sm-none" :disabled="isVoting"
                           @click="rightWin"><i class="fa-solid fa-2x fa-thumbs-up"></i>
                           <p class="d-inline-block m-0" v-if="showGameRoomVotes">
-                            @{{ rightVotes }}&nbsp;(@{{ rightVotesPercentage }}%)
+                            <I-Count-Up :end-val="rightVotes"></I-Count-Up>
+                            (<I-Count-Up :end-val="rightVotesPercentage"></I-Count-Up>%)
                           </p>
                         </button>
                       </div>
