66e561435447c9d47cd9c3c5e7185965b77848e1
bill <bill@robowebtech.com>
2024-07-22 19:39:29 +0800

    feat: Add NSFW alert message for different languages fix: hide scrollbar for firefox

--- resources/js/components/partials/ImageMask.vue
+++ resources/js/components/partials/ImageMask.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="image-mask" v-if="mask">
     <div>{{ $t('post.nsfw') }}</div>
+    <h6 class="mt-2">{{ $t('post.nsfw.alert') }}</h6>
     <div class="btn btn-outline-light" @click="show"><i class="fa-solid fa-eye"></i>&nbsp;{{ $t('post.nsfw.show') }}</div>
   </div>
 </template>
