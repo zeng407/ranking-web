5cc46d89225abd487a3b088d7ef22e8e689a6c80
bill <zeng.cae37@gmail.com>
2025-06-21 18:38:22 +0800

    feat: Add ad removal functionality and related components

---
+++ app/Http/Controllers/Post/AdController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers\Post;
+
+
+use App\Http\Controllers\Controller;
+
+class AdController extends Controller
+{
+
+    public function onead_media()
+    {
+        return view('ads.remove-ad');
+    }
+
+}
