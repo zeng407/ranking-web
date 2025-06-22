826e8ccbed72cdb649eeb7334baeb0a864f7d4eb
bill <zeng.cae37@gmail.com>
2025-06-22 10:58:50 +0800

    feat: Add Ezoic configuration and scripts for consent management

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,16 @@
     </script>
   @endif
 
+  @if (config('services.ezoic.enabled'))
+    <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+    <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+    <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+    <script>
+      window.ezstandalone = window.ezstandalone || {};
+      ezstandalone.cmd = ezstandalone.cmd || [];
+    </script>
+  @endif
+
   {{-- Google ad --}}
   @if (config('services.google_ad.enabled'))
     <script async
