b0ac6d7cea8ee3cd8edc6c02101e21c3c16cb791
bill <zeng.cae37@gmail.com>
2025-06-24 13:03:58 +0800

    fix: Refactor cookie handling for ad click tracking and improve cookie management functions

--- resources/views/ads/remove-ad.blade.php
+++ resources/views/ads/remove-ad.blade.php
@@ -189,7 +189,16 @@
                 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
               }
             }).then(function() {
-              this.$cookies.set('_ad_click', 1, '1h');
+              function setCookie(name, value, hours) {
+                let expires = "";
+                if (hours) {
+                  const date = new Date();
+                  date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+                  expires = "; expires=" + date.toUTCString();
+                }
+                document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
+              }
+              setCookie('_ad_click', 1, 1);
             });
 
             clickable.forEach(function(c) {
@@ -203,7 +212,18 @@
   <script src="https://ad-specs.guoshipartners.com/static/js/ad-serv.min.js"></script>
 
   <script>
-    if (this.$cookies.get('_ad_click')) {
+    function getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+      return null;
+    }
+
+    function removeCookie(name) {
+      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    }
+
+    if (getCookie('_ad_click')) {
       // 建立提示元素
       const tip = document.createElement('div');
       tip.style.position = 'fixed';
@@ -229,6 +249,7 @@
       const btn = document.createElement('button');
       btn.className = 'btn btn-secondary rounded-pill shadow';
       btn.onclick = function() {
+        removeCookie('_ad_click');
         location.reload();
       };
       const icon = document.createElement('i');
@@ -241,6 +262,7 @@
       tip.appendChild(text);
       tip.appendChild(btn);
       document.body.appendChild(tip);
+
     }
   </script>
 @endif
