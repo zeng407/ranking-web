cd353ca74c7ca1db50d4a0d4d97a9c4e557364de
bowei, zeng <zeng.cae37@gmail.com>
2025-08-27 14:55:09 +0800

    feat: Add laradock configuration options

--- config/services.php
+++ config/services.php
@@ -66,6 +66,7 @@ return [
         'rank_page' => env('ONEAD_RANK_PAGE', false),
     ],
 
+    // google auth login
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
