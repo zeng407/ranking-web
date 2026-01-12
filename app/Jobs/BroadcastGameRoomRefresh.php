1374ddb455e6d8fc8e2511190e87280ac3b5413a
bill <zeng.cae37@gmail.com>
2026-01-13 07:21:55 +0800

    Refactor NotifyGameRoomRefresh listener to remove queueing and simplify constructor

--- app/Jobs/BroadcastGameRoomRefresh.php
+++ app/Jobs/BroadcastGameRoomRefresh.php
@@ -24,6 +24,7 @@ class BroadcastGameRoomRefresh implements ShouldBroadcastNow
      */
     public function __construct(GameRoom $gameRoom)
     {
+        $this->onQueue('game_room');
         $this->gameRoom = $gameRoom;
     }
 
